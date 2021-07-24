ac5afff51e910891765b33cf2eebc86b499dbf9d
Joseph Guenther <joseph@gtinc.ca>
2021-07-24 13:58:03 -0400

    Redid UI. Changed compiler.

--- IDEDemo/Tabs/TaskListTab.vb
+++ IDEDemo/Tabs/TaskListTab.vb
@@ -3,4 +3,33 @@
 Public Class TaskListTab
     Inherits FATabStripItem
 
+    Friend WithEvents dgvTasks As DataGridView
+
+    Private Sub InitializeComponent()
+        Me.dgvTasks = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvTasks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvTasks
+        '
+        Me.dgvTasks.AllowUserToAddRows = False
+        Me.dgvTasks.AllowUserToDeleteRows = False
+        Me.dgvTasks.AllowUserToResizeRows = False
+        Me.dgvTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTasks.Location = New System.Drawing.Point(0, 0)
+        Me.dgvTasks.Name = "dgvTasks"
+        Me.dgvTasks.ReadOnly = True
+        Me.dgvTasks.RowHeadersVisible = False
+        Me.dgvTasks.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.dgvTasks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.dgvTasks.ShowCellErrors = False
+        Me.dgvTasks.ShowCellToolTips = False
+        Me.dgvTasks.ShowEditingIcon = False
+        Me.dgvTasks.ShowRowErrors = False
+        Me.dgvTasks.Size = New System.Drawing.Size(240, 150)
+        Me.dgvTasks.TabIndex = 0
+        CType(Me.dgvTasks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
 End Class
